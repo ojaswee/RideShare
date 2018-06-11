eb17fe6d5e27e57d2892dd7e22a55841987dbadb
Ojaswee Dhungana <ojaswee@outlook.com>
2018-06-11 17:54:08 -0500

    comment added

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
+<!-- This file is used to connect to the database 
+Name of my database is rideShare--> 
 <link type="text/css" href="style.css" rel="stylesheet">
 <?php
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'car');
+   define('DB_DATABASE', 'rideShare');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
